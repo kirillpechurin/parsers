db90ccd358ccee96b1455ec78179dac5f1cc0caf
Kirill <k.pechurin02@mail.ru>
2022-01-06 12:57:32 +0300

    task002 | add all routers to main app

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 
 from src.bootstrap.server import Server
+from app.routers import api_routers
 
 app = FastAPI()
 
 
 def init_app():
     Server.set_app(app)
+    Server.set_routers(api_routers)
 
 
 init_app()
