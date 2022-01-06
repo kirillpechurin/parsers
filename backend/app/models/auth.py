e0f96620092b66639c6464a0c8ff40519625f7aa
Kirill <k.pechurin02@mail.ru>
2022-01-06 13:19:43 +0300

    task002 | create auth signup view

--- backend/app/models/auth.py
+++ backend/app/models/auth.py
@@ -1,3 +1,5 @@
+import datetime
+
 from pydantic import EmailStr, BaseModel
 
 
@@ -5,3 +7,20 @@ class AuthRegisterData(BaseModel):
     email: EmailStr
     password: str
     repeat_password: str
+
+
+class Account(BaseModel):
+    account_id: str
+    email: EmailStr
+    first_name: str = ""
+    last_name: str = ""
+    confirmed: bool = False
+    created_at: datetime.datetime = None
+
+    @classmethod
+    def parse_obj(cls, obj: dict):
+        data = {}
+        if obj.get("_id"):
+            data["account_id"] = str(obj.pop("_id"))
+        data.update(obj)
+        return super(Account, cls).parse_obj(data)
--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,8 +1,21 @@
 from fastapi import APIRouter
 
+from app.models.auth import AuthRegisterData, Account
+from src.biz.services.auth_services.auth import AuthService
+
 auth_router = APIRouter(
     prefix="/auth",
     tags=["auth"],
 )
 
 
+@auth_router.post('/signup')
+async def signup(auth_data: AuthRegisterData):
+    account = AuthService().create_account(
+        email=auth_data.email,
+        password=auth_data.password,
+        repeat_password=auth_data.repeat_password
+    )
+    return {
+        "data": account
+    }
