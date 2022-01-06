e3251751e30381bf0f890e735cde1812c8ea8ba3
Kirill <k.pechurin02@mail.ru>
2022-01-06 20:51:12 +0300

    task007 | create handooks examples-maps

--- backend/app/routers/handbooks.py
+++ backend/app/routers/handbooks.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from app.models.responses.wrap import WrapModel
-from src.biz.services.handbooks_services.cities import CityService
+from src.biz.services.handbooks_services.city import CityService
+from src.biz.services.handbooks_services.example import ExampleService
+
 
 handbooks_router = APIRouter(
     prefix="/handbooks",
@@ -14,4 +16,15 @@ async def list_cities():
     cities = CityService().get_cities()
     return WrapModel(
         data=cities
+    )
+
+
+@handbooks_router.get("/examples/maps")
+async def examples():
+    map_examples = ExampleService().get_maps_examples()
+    return WrapModel(
+        data={
+            "type": "maps",
+            "examples": map_examples
+        }
     )
