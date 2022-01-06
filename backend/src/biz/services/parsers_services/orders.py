774060152355cf716bf71300ec041a7f6f87cab6
Kirill <k.pechurin02@mail.ru>
2022-01-06 21:26:28 +0300

    task010 | add service methods on create order

--- backend/src/biz/services/parsers_services/orders.py
+++ backend/src/biz/services/parsers_services/orders.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+import pytz
 from bson import ObjectId
 
 from src.biz.services.base_service import BaseService
@@ -11,3 +14,12 @@ class OrderService(BaseService):
 
     def update_status_order(self, order_id: str):
         self.collection.update_one({"_id": ObjectId(order_id)}, {"$set": {"status_ready": True}})
+
+    def create_order(self, data, email):
+        order = self.collection.insert_one({
+            "data": data,
+            "email": email,
+            "created_at": datetime.now(tz=pytz.UTC),
+            "status_ready": False
+        })
+        return str(order.inserted_id)
