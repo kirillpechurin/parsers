a89a056d8d72c9dc0e9fa21e99e2449a27f1246b
Kirill <k.pechurin02@mail.ru>
2022-01-06 14:14:20 +0300

    task002 | create connection to mongodb

--- backend/src/biz/services/base_service.py
+++ backend/src/biz/services/base_service.py
@@ -1,11 +1,13 @@
+import os
+
 import pymongo
 
 
 class BaseService:
 
     def __init__(self):
-        self.client = pymongo.MongoClient(host="localhost",
+        self.client = pymongo.MongoClient(host="mongodb",
                                           port=27017,
-                                          username="kirill",
-                                          password="k1979082002")
+                                          username=os.environ.get("MONGO_DB_USERNAME"),
+                                          password=os.environ.get("MONGO_DB_PASSWORD"))
         self.db_name = self.client['parsers_db']
