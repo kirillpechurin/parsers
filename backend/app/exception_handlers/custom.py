837000444bed04607f58f7cd299fb8fb5cbcfbf2
Kirill <k.pechurin02@mail.ru>
2022-01-07 13:46:55 +0300

    task014 | review by custom exception handlers, add docs, typing

--- backend/app/exception_handlers/custom.py
+++ backend/app/exception_handlers/custom.py
@@ -6,14 +6,28 @@ from starlette import status
 from src.biz.exceptions.custom import APIException
 
 
-async def api_exception_handler(request: Request, exc: APIException):
+async def api_exception_handler(request: Request, exc: APIException) -> JSONResponse:
+    """
+    Обработчик пользовательских ошибок
+
+    :param request: Request instance FastAPI
+    :param exc: custom APIException
+    :return: status_code and content error
+    """
     return JSONResponse(
         status_code=exc.status_code,
         content=exc.exc_object
     )
 
 
-async def http_exception_handler(request: Request, exc: HTTPException):
+async def http_exception_handler(request: Request, exc: HTTPException) -> JSONResponse:
+    """
+    Обработчик ошибок fastAPI.HTTPException
+
+    :param request: Request instance FastAPI
+    :param exc: fastAPI.HTTPException
+    :return: status_code and content в нужном виде
+    """
     return JSONResponse(
         status_code=exc.status_code,
         content={
@@ -23,7 +37,14 @@ async def http_exception_handler(request: Request, exc: HTTPException):
     )
 
 
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
+async def validation_exception_handler(request: Request, exc: RequestValidationError) -> JSONResponse:
+    """
+    Обработчик ошибок встроенной валидации
+
+    :param request: Request instance FastAPI
+    :param exc: fastAPI.exceptions.RequestValidationError
+    :return: status_code and content в нужном виде
+    """
     error_dict = exc.errors()[0]
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
