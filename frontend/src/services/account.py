d3a55cd00a624e156c819a88357b1d310e0a258d
Kirill <k.pechurin02@mail.ru>
2022-01-07 17:54:42 +0300

    frontend001 | review

--- frontend/src/services/account.py
+++ frontend/src/services/account.py
@@ -3,6 +3,12 @@ from src.services.request_service import RequestService
 SIGNUP_PREFIX = "/auth/signup"
 LOGIN_PREFIX = "/auth/login"
 CHECK_TOKEN_PREFIX = "/auth/check_token"
+CONFIRM_ACCOUNT_PREFIX = "/auth/confirm/{}"
+FORGOT_PASSWORD_PREFIX = "/auth/password/forgot"
+RESET_PASSWORD_PREFIX = "/auth/password/reset/{}"
+ACCOUNT_DETAIL_PREFIX = "/auth/me"
+UPDATE_ACCOUNT_PREFIX = '/auth/me/update'
+RESET_PASSWORD_AUTH_PREFIX = '/auth/password/reset/auth'
 
 
 class AccountService:
@@ -30,42 +36,42 @@ class AccountService:
 
     @staticmethod
     def confirm_email(account_id):
-        response = RequestService.post(f"/auth/confirm/{account_id}")
+        response = RequestService.post(CONFIRM_ACCOUNT_PREFIX.format(account_id))
         if response.status_code != 204:
             return None, response.json()['detail']
         return None, None
 
     @staticmethod
     def forgot_password(data):
-        response = RequestService.post("/auth/password/forgot", data=data)
+        response = RequestService.post(FORGOT_PASSWORD_PREFIX, data=data)
         if response.status_code != 204:
             return None, response.json()['detail']
         return None, None
 
     @staticmethod
     def reset_password(account_id, data):
-        response = RequestService.post(f"/auth/password/reset/{account_id}", data=data)
+        response = RequestService.post(RESET_PASSWORD_PREFIX.format(account_id), data=data)
         if response.status_code != 204:
             return None, response.json()['detail']
         return None, None
 
     @staticmethod
     def get_detail(x_token):
-        response = RequestService.get_auth("/auth/me", x_token)
+        response = RequestService.get_auth(ACCOUNT_DETAIL_PREFIX, x_token)
         if response.status_code != 200:
             return None, response.json()['detail']
         return response.json()['data'], None
 
     @staticmethod
     def update_account(data, x_token):
-        response = RequestService.patch_auth('/auth/me/update', data=data, x_token=x_token)
+        response = RequestService.patch_auth(UPDATE_ACCOUNT_PREFIX, data=data, x_token=x_token)
         if response.status_code != 204:
             return None, response.json()['detail']
         return None, None
 
     @staticmethod
     def update_password(data, x_token):
-        response = RequestService.post_auth('/auth/password/reset/auth', data=data, x_token=x_token)
+        response = RequestService.post_auth(RESET_PASSWORD_AUTH_PREFIX, data=data, x_token=x_token)
         if response.status_code != 204:
             return None, response.json()['detail']
         return None, None
