0af82c4402eedee1413f57ba46bb9ae25c722004
Kirill <k.pechurin02@mail.ru>
2022-01-07 15:52:30 +0300

    task015 | create task on mail send

--- backend/src/cel/tasks.py
+++ backend/src/cel/tasks.py
@@ -1,4 +1,5 @@
 import os
+from typing import Union
 
 from celery import Celery
 
@@ -6,7 +7,7 @@ from src.biz.services.parsers_services.orders import OrderService
 from src.biz.services.parsers_services.maps.map_reviews import MapReviewsService
 
 from .utils import get_map_by_name, get_driver
-
+from src.biz.services.mail.sender import MailService
 
 app = Celery("tasks", backend='rpc://', broker="amqp://guest:guest@rabbitmq:5672//")
 DIRECTORY_STORAGE_MAPS_RESULT = "storage/parsers/maps/"
@@ -28,3 +29,13 @@ def map_parser_task(map_name, data: dict, order_id: str):
     MapReviewsService().save_reviews(data=reviews, html_filename=new_html_filename, order_id=order_id)
 
     return "Success parsing review from {}".format(map_name)
+
+
+@app.task
+def send_on_email(to_email: str, subject: str, body: Union[list, str]):
+    MailService.send(
+        to=to_email,
+        subject=subject,
+        contents=[body] if isinstance(body, str) else body
+    )
+    return "Success send on email"
