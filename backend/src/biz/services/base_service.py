9b91c881a6e884744cf97e4b681b5ee9dd52e97a
Kirill <k.pechurin02@mail.ru>
2022-01-07 16:50:42 +0300

    task016 | test parsing

--- backend/src/biz/services/base_service.py
+++ backend/src/biz/services/base_service.py
@@ -16,6 +16,7 @@ class BaseService:
                                               port=27017,
                                               username=os.environ.get("MONGO_DB_USERNAME"),
                                               password=os.environ.get("MONGO_DB_PASSWORD"))
-            self.db_name = self.client[os.environ.get("MONGO_DB_NAME")]
         except:
             raise InternalError(message="Ошибка соединения с БД")
+
+        self.db_name = self.client[os.environ.get("MONGO_DB_NAME")]
