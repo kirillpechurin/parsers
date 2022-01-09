4b58a4b6aaca7ae73ded913cdfe956b0508883a6
Kirill <k.pechurin02@mail.ru>
2022-01-09 20:01:12 +0300

    dev_site | create docs main api

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,19 @@ from src.bootstrap.server import Server
 from app.routers import api_routers
 from app.exception_handlers import handlers
 
-app = FastAPI()
+app = FastAPI(
+    title="Parsers API",
+    description="Documentation api parsers",
+    version="1",
+    contact={
+        "name": "Kirill Pechurin",
+        "email": "k.pechurin04@gmail.com"
+    },
+    docs_url="/api/v1/docs",
+    redoc_url="/api/v1/redoc",
+    openapi_url="/api/v1/openapi.json",
+)
+
 origins = [
     "http://localhost:5000",
     "localhost:5000"
